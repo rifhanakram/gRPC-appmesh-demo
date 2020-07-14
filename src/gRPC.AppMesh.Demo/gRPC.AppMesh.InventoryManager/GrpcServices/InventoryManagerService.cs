7d6ba1cc107fd949a366e8b33255b544f6b86930
Rifhan <rifhan.akram1@gmail.com>
2020-07-15 03:07:12 +0530

    Add v2 changes to inventorymanager

--- src/gRPC.AppMesh.Demo/gRPC.AppMesh.InventoryManager/GrpcServices/InventoryManagerService.cs
+++ src/gRPC.AppMesh.Demo/gRPC.AppMesh.InventoryManager/GrpcServices/InventoryManagerService.cs
@@ -14,7 +14,9 @@ namespace gRPC.AppMesh.InventoryManager.GrpcServices
             
             return Task.FromResult(new InventoryAdjustmentResponse
             {
-                AvailableQuantity = ( randomStockCount.Next(1000,2000) - request.Quantity)
+                AvailableQuantity = ( randomStockCount.Next(1000,2000) - request.Quantity),
+                Message = $"{request.TypeOfAdjustment} was made for Quantity : {request.Quantity} on " +
+                          $"ProductId : {request.ProductId}"
             });
         }
     }
